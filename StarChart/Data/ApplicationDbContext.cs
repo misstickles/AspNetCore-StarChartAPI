56629401e048c3c7e6f7b9821bdf2dcb075ff4e9
misstickles <jofaircloth@gmail.com>
2019-08-17 22:03:39 +0100

    Added Muddle 3

--- StarChart/Data/ApplicationDbContext.cs
+++ StarChart/Data/ApplicationDbContext.cs
@@ -1,11 +1,14 @@
-﻿using Microsoft.EntityFrameworkCore;
-
-namespace StarChart.Data
+﻿namespace StarChart.Data
 {
+    using Microsoft.EntityFrameworkCore;
+    using StarChart.Models;
+
     public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
         }
+
+        public DbSet<CelestialObject> CelestialObjects { get; set; }
     }
 }
