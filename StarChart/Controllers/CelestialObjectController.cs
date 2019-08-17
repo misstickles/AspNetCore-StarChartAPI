2594b07eb0484539435c2ca58bf6be39eb0c114b
misstickles <jofaircloth@gmail.com>
2019-08-17 22:32:02 +0100

    Added proper Muddle for #5 - didn't get this one, side effects in a Get made my head hurt and confused.

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -1,5 +1,6 @@
 ﻿namespace StarChart.Controllers
 {
+    using System.Linq;
     using Microsoft.AspNetCore.Mvc;
     using StarChart.Data;
 
@@ -12,6 +13,48 @@
         public CelestialObjectController(ApplicationDbContext db)
         {
             _context = db;
+        }
+
+        [HttpGet("{id:int}", Name = "GetById")]
+        public IActionResult GetById(int id)
+        {
+            var objects = _context.CelestialObjects.Find(id);
+
+            if (objects == null) return NotFound();
+
+            objects.Satellites = _context.CelestialObjects.Where(c => c.OrbitedObjectId == id).ToList();
+
+            return Ok(objects);
+        }
+
+        [HttpGet("{name}")]
+        public IActionResult GetByName(string name)
+        {
+            var objects = _context.CelestialObjects.Where(c => c.Name == name);
+
+            if (!objects.Any()) return NotFound();
+
+            foreach (var obj in objects)
+            {
+                obj.Satellites = _context.CelestialObjects.Where(c => c.Name == name).ToList();
+            }
+
+            return Ok(objects);
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var objects = _context.CelestialObjects.ToList();
+
+            if (!objects.Any()) return NotFound();
+
+            foreach (var obj in objects)
+            {
+                obj.Satellites = _context.CelestialObjects.Where(c => c.Name == obj.Name).ToList();
+            }
+
+            return Ok(objects);
         }
     }
 }
