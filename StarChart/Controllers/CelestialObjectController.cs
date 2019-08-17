3cb16777f0dfdfadcc4a4af6ac4327a441dd2224
misstickles <jofaircloth@gmail.com>
2019-08-17 22:09:14 +0100

    Added Muddle 4 (controller)

---
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -0,0 +1,17 @@
+﻿namespace StarChart.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using StarChart.Data;
+
+    [Route("")]
+    [ApiController]
+    public class CelestialObjectController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CelestialObjectController(ApplicationDbContext db)
+        {
+            _context = db;
+        }
+    }
+}
